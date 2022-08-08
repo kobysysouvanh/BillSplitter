94a2d78f8851d63d1222b40bd578088cff1ade01
kobysysouvanh <kobysysouvanh@gmail.com>
2022-08-08 02:02:20 -0400

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [bill, setBill] = useState();
+  const [tip, setTip] = useState();
+  const [total, setTotal] = useState();
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    if (count > 1){
+      setCount(count - 1);    
+    }
+  };
+
+  const grabBill = event => {
+    setBill(event.target.value);
+    
+  };
+
+  const grabTip = event => {
+    setTip(event.target.value/100);
+  };
+
+  useEffect(() => {
+    if (bill && tip != null){
+      setTotal((parseInt(bill) + parseInt((tip*parseInt(bill))))/ parseInt(count));
+    }
+    else {
+      setTotal(0);
+    }
+  }, [bill, tip, count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 id="title">Bill Splitter</h1>
+      <div className="label" id="total">Total</div>
+      <div className="inputContainer">
+        <span className="inputAmount">$<input className="totalInput" type="text" placeholder="0.00" onKeyUp={grabBill}/></span>     
+      </div>
+      <div className="label" id="tip">Tip</div>
+      <div className="inputContainer">
+        <span className="inputAmount">%<input className="tipInput" type="text" placeholder="0" onKeyUp={grabTip}/></span>
+      </div>
+      <span className="label" id="people">People</span>
+      <div className="buttonContainer">
+        <button className="button" onClick={decreaseCount}>-</button>
+        <span className="numberOfPeople" >{count}</span>
+        <button className="button" onClick={increaseCount}>+</button>
+      </div>
+      <div className="totalContainer">
+        <label className="label" id="totalPerPerson">Cost Per Person</label>
+        <label className="label">${total}</label>
+      </div>
     </div>
   );
 }
-
 export default App;
